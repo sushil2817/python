96eb2604e360ab9fc85998cfa9fea7820acaed58
Sushil Kumawat <79733926+sushil2817@users.noreply.github.com>
2024-02-12 21:54:47 +0530

    learned about python

--- python_shell.py
+++ python_shell.py
@@ -1,2 +1,26 @@
-# in terminal type -> python (now you working in python shell)
+ # in terminal type -> python (now you working in python shell)
     
+# print("chai") 
+
+# 2*2 #4   
+# chai = 3
+# chai # 3
+
+# "chai"*4 # 'chaichaichaichai'
+# "chai "*4 #'chai chai chai chai '
+# score = 100 #score 100
+# tea
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# NameError: name 'tea' is not defined
+
+# import os
+# os.getcwd()
+
+# for ch in "chai":
+# ...     print(ch)
+# ... 
+
+# sys.platform()
+
+# import hello_world                
